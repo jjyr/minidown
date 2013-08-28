ee5910f6c051e8c474cb21a842abe7f254c98b99
jjy <jjyruby@gmail.com>
2013-08-28 15:38:55 +0800

    complete link syntax

--- lib/minidown/elements/text_element.rb
+++ lib/minidown/elements/text_element.rb
@@ -4,6 +4,12 @@ module Minidown
   class TextElement < Element
     EscapeChars = %w{# > * + \- `}
     EscapeRegexp = /\\([#{EscapeChars.join '|'}])/
+    
+    Regexp = {
+      link: /\[(.+)\]\((.+)\)/,
+      link_title: /\ &quot;(.+)&quot;/,
+      link_url: /(\S+)/
+    }
 
     attr_accessor :escape, :convert
     
@@ -20,7 +26,24 @@ module Minidown
     def content
       str = super
       str.gsub!(EscapeRegexp, '\\1') if escape
-      CGI.escape_html str
+      str = CGI.escape_html str
+      convert_str(str) if convert
+      str
+    end
+
+    def convert_str str
+      str.gsub! Regexp[:link] do
+        text, url = $1, $2
+        url =~ Regexp[:link_title]
+        title = $1
+        url =~ Regexp[:link_url]
+        url = $1
+        attr = {href: url}
+        attr[:title] = title if title
+        build_tag 'a', attr do |content|
+          content << text
+        end
+      end
     end
 
     def paragraph
--- spec/text_element_spec.rb
+++ spec/text_element_spec.rb
@@ -8,16 +8,12 @@ describe Minidown do
           str = %Q{This is [an example](http://example.com/ "Title") inline link.
 
 [This link](http://example.net/) has no title attribute.}
-          Minidown.parse(str).to_html.should == %Q{<p>This is <a href="http://example.com/" title="Title">
-an example</a> inline link.</p>
-
-<p><a href="http://example.net/">This link</a> has no
-title attribute.</p>}
+          Minidown.parse(str).to_html.should == %Q{<p>This is <a href="http://example.com/" title="Title">an example</a> inline link.</p><p><a href="http://example.net/">This link</a> has no title attribute.</p>}
         end
 
         it 'should allow related path' do
           str = "See my [About](/about/) page for details."
-          Minidown.parse(str).to_html.should == %Q{<p>See my <a href=\"/about/\">About<a/> page for details.</p>}
+          Minidown.parse(str).to_html.should == %Q{<p>See my <a href=\"/about/\">About</a> page for details.</p>}
         end
 
         it 'should allow reference' do
