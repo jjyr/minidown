25c189ebe4c1d1c6aeb33e8850ce3db9e00b6e57
jjy <jjyruby@gmail.com>
2013-08-30 16:05:30 +0800

    add a broken spec

---
+++ spec/other_parse_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Minidown do
+  describe '.parse' do
+    describe 'nest syntax' do
+      it 'should parse correct' do
+        str = "#### [ruby](http://ruby-lang.org/)\n---"
+        Minidown.parse(str).should == "<h4><a href=\"http://ruby-lang.org/\">ruby</a></h4><hr>"
+      end
+    end
+  end
+end
