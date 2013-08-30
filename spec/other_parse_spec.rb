977b781d839e57914dd14fef7a4d0e47cf833419
jjy <jjyruby@gmail.com>
2013-08-30 16:23:37 +0800

    fix specs

--- spec/other_parse_spec.rb
+++ spec/other_parse_spec.rb
@@ -5,7 +5,7 @@ describe Minidown do
     describe 'nest syntax' do
       it 'should parse correct' do
         str = "#### [ruby](http://ruby-lang.org/)\n---"
-        Minidown.parse(str).should == "<h4><a href=\"http://ruby-lang.org/\">ruby</a></h4><hr>"
+        Minidown.parse(str).to_html.should == "<h4><a href=\"http://ruby-lang.org/\">ruby</a></h4><hr>"
       end
     end
   end
